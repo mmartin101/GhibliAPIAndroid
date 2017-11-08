d50d4a09988595c5e6f47fc3382ca165aa88f77e
Max Martin <mmartin101@gmail.com>
2017-11-07 22:42:52 -0800

    remove butterknife

---
+++ app/src/main/java/com/mmartin/ghibliapi/film/FilmCardViewHolder.kt
@@ -0,0 +1,20 @@
+package com.mmartin.ghibliapi.film
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.view_film_list_item.view.*
+
+/**
+ * View holder for Film models
+ *
+ *
+ * Created by mmartin on 4/10/17.
+ */
+
+class FilmCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bindData(film: Film) {
+        itemView.film_title_text_view.text = film.title
+        itemView.film_description_text_view.text = film.description
+        itemView.film_release_date_text_view.text = film.releaseDate
+    }
+}
